d5a7afab97a704045002c072b7a252dcbfc517b8
szabolevi16 <szabolevi0000@gmail.com>
2024-08-08 14:39:55 +0200

    Creation

---
+++ I4P_feladat_project/I4P_feladat_project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I4P_feladat_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
