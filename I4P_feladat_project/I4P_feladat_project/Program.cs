7bc88f5a6d4e781d6faff231ebcc8c277aa1b14a
szabolevi16 <szabolevi0000@gmail.com>
2024-08-11 16:10:53 +0200

    rejtjelfejto

--- I4P_feladat_project/I4P_feladat_project/Program.cs
+++ I4P_feladat_project/I4P_feladat_project/Program.cs
@@ -14,26 +14,18 @@ namespace I4P_feladat_project
             Console.WriteLine("Üdvözlöm a rejtjelezési programban!");
             Console.WriteLine("Kérem adja meg a rejtjelezni kívánt üzenetet az angol abc kisbetűivel:");
             string uz = Console.ReadLine();
-            string kulcs = kulcsgenerator(uz.Length);
-            Console.WriteLine($"A legenerált rejtjelezési kulcs: {kulcs}");
-            string rejtuz = rejtjelezes(uz, kulcs);
-            Console.WriteLine($"A rejtjelezett üzenet: {rejtuz}");
+            Console.WriteLine($"Kérem adja meg a kulcsot:");
+            string kulcs = Console.ReadLine();
+            if (kulcs.Length<uz.Length) Console.WriteLine("A kulcs rövidebb az üzenetnél");
+            else 
+            {
+                string rejtuz = rejtjelezes(uz, kulcs);
+                Console.WriteLine($"A rejtjelezett üzenet: {rejtuz}");
+                Console.WriteLine();
+                Console.WriteLine($"A visszafejtett üzenet: {rejtjelfejto(rejtuz,kulcs)}");
+            }
+
             Console.ReadKey();
-        }
-        static string kulcsgenerator(int hossz)
-        {
-            Random r = new Random();
-            char[] c = new char[hossz];
-            string kulcs = "";
-            for (int i = 0; i < hossz; i++)
-            {
-                c[i] = (char)r.Next(97, 123);
-            }
-            for (int i = 0;i < c.Length; i++)
-            {
-                kulcs= kulcs + c[i];
-            }
-            return kulcs;
         }
         static string rejtjelezes(string uzenet, string kulcs)
         {
@@ -41,7 +33,9 @@ namespace I4P_feladat_project
             int[] t = new int[uzenet.Length];
             for (int i = 0; i < uzenet.Length; i++)
             {
-                if (uzenet[i]==' ') t[i] = ((int)uzenet[i] - 6) + ((int)kulcs[i] - 97);
+                if (uzenet[i]==' ' && kulcs[i]==' ') t[i] = ((int)uzenet[i] - 6) + ((int)kulcs[i] - 6);
+                else if (uzenet[i] == ' ' && kulcs[i] != ' ') t[i] = ((int)uzenet[i] - 6) + ((int)kulcs[i] - 97);
+                else if (uzenet[i] != ' ' && kulcs[i] == ' ') t[i] = ((int)uzenet[i] - 97) + ((int)kulcs[i] - 6);
                 else t[i] = ((int)uzenet[i] - 97) + ((int)kulcs[i] - 97);
             }
             for (int j = 0; j < t.Length; j++)
@@ -52,6 +46,26 @@ namespace I4P_feladat_project
                 rejtuz = rejtuz + (char)t[j];
             }
             return rejtuz;
+        }
+        static string rejtjelfejto(string rejtuz, string kulcs)
+        {
+            string uzenet = "";
+            int[] t = new int[rejtuz.Length];
+            for (int i = 0; i < rejtuz.Length; i++)
+            {
+                if (rejtuz[i] == ' ' && kulcs[i] == ' ') t[i] = ((int)rejtuz[i] - 6) - ((int)kulcs[i] - 6);
+                else if (rejtuz[i] == ' ' && kulcs[i] != ' ') t[i] = ((int)rejtuz[i] - 6) - ((int)kulcs[i] - 97);
+                else if (rejtuz[i] != ' ' && kulcs[i] == ' ') t[i] = ((int)rejtuz[i] - 97) - ((int)kulcs[i] - 6);
+                else t[i] = ((int)rejtuz[i] - 97) - ((int)kulcs[i] - 97);
+            }
+            for (int j = 0; j < t.Length; j++)
+            {
+                if (t[j] < 0) t[j] = t[j] + 27;
+                if (t[j] == 26) t[j] = t[j] + 6;
+                else if (t[j] != 26) t[j] = t[j] + 97;
+                uzenet = uzenet + (char)t[j];
+            }
+            return uzenet;
         }
     }
 }
